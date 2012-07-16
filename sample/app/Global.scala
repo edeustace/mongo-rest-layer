2e0e831aefe3d39e233e9cef9c7a35a9330a868f
ed <ed.eustace@gmail.com>
2012-07-16 17:52:42 +0300

    Update master

--- sample/app/Global.scala
+++ sample/app/Global.scala
@@ -4,8 +4,6 @@ import play.api.mvc.{Handler, RequestHeader, Request}
 object Global extends GlobalSettings {
 
 
-  val mongoLab = "mongodb://corespring:iFt4inav@ds033307.mongolab.com:33307/metadata"
-  val mongoUri = "mongodb://test:test@localhost:27017/content-tagger-dev"
   val mongoRestLayer = new MongoRestLayer("/mongo-rest", mongoUri)
 
   override def onRouteRequest(request:RequestHeader) : Option[Handler] = {
