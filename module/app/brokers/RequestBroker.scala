9e604677a895b0b908d6ff70763df95cfccd5a92
ed <ed.eustace@gmail.com>
2012-10-12 12:29:11 +0300

    Update module/app/brokers/RequestBroker.scala

--- module/app/brokers/RequestBroker.scala
+++ module/app/brokers/RequestBroker.scala
@@ -4,10 +4,19 @@ import play.api.mvc.{AnyContent, Action, RequestHeader}
 
 trait RequestBroker{
   /**
+   * @deprecated - use handle2 for now.
    * Matches a request with a result string.
    * @param request the request to handle
    * @return either the string result or None if it can't handle the request
    */
+  
   def handle(request:RequestHeader) : Option[String]
+
+  /**
+   * Matches a request with an Action
+   * @param request the request to handle
+   * @return Some Action if found.
+   */
+
   def handle2(request:RequestHeader) : Option[Action[AnyContent]]
 }
